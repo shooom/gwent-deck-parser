051b8a67fdbd8623eb498d5bd062273cd8e125ba
dikasyanov <dikanemo@gmail.com>
2022-03-14 22:03:46 +0100

    init commit

---
+++ src/main/java/com/parser/gwentdeckparser/GwentDeckParserApplication.java
@@ -0,0 +1,13 @@
+package com.parser.gwentdeckparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwentDeckParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GwentDeckParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parser/gwentdeckparser/GwentDeckParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parser.gwentdeckparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwentDeckParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
