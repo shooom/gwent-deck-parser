4b85431aa95bf8e6eec5f8caad5531081b7cd607
dikasyanov <dikanemo@gmail.com>
2022-04-11 22:58:42 +0200

    added some classes for storing cards

---
+++ src/main/java/com/parser/gwentdeckparser/api/CardsStorageController.java
@@ -0,0 +1,23 @@
+package com.parser.gwentdeckparser.api;
+
+import com.parser.gwentdeckparser.cardStorage.model.CardDocument;
+import com.parser.gwentdeckparser.cardStorage.service.CardStorageMdbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/cards")
+public class CardsStorageController {
+
+    private final CardStorageMdbService storageService;
+
+    @GetMapping
+    public ResponseEntity<CardDocument> getByName(@RequestParam(name = "name") String name) {
+        return ResponseEntity.ok(storageService.getByName(name));
+    }
+}
