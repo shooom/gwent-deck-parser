6547ce406e8c35e011fc1be9c66fff3dcc363739
dikasyanov <dikanemo@gmail.com>
2022-07-15 00:18:27 +0200

    [API] did some refactoring in controllers (need to update tests)

--- src/main/java/com/parser/gwentdeckparser/api/CardsParserController.java
+++ src/main/java/com/parser/gwentdeckparser/api/CardsParserController.java
@@ -2,40 +2,31 @@ package com.parser.gwentdeckparser.api;
 
 import com.parser.gwentdeckparser.cardStorage.service.CardStorageMdbService;
 import com.parser.gwentdeckparser.deckGraber.CardGrabberService;
-import com.parser.gwentdeckparser.deckGraber.DeckGraberService;
 import com.parser.gwentdeckparser.deckStructure.deckBuilder.Card;
-import com.parser.gwentdeckparser.deckStructure.guide.Guide;
-import com.parser.gwentdeckparser.deckStructure.guide.GuideList;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("deck")
-@RequiredArgsConstructor
-public class ParserController {
-    private final DeckGraberService deckGrabber;
+@RequestMapping("/cards")
+public class CardsParserController {
     private final CardGrabberService cardGrabber;
     private final CardStorageMdbService storageService;
 
-    @GetMapping("/top")
-    public GuideList getTopDecks(@RequestParam(value = "deckNum", defaultValue = "3") long deckNum) {
-        return deckGrabber.getTopDecks(deckNum);
+    @Autowired
+    CardsParserController(CardGrabberService cardGrabber, CardStorageMdbService storageService) {
+        this.cardGrabber = cardGrabber;
+        this.storageService = storageService;
     }
 
-    @GetMapping("/{deckId}")
-    public Guide getDeck(@PathVariable(value = "deckId") long deckId) {
-        return deckGrabber.getDeckById(deckId);
-    }
-
-    @GetMapping("/cards")
+    @GetMapping
     public List<Card> getCards(@RequestParam Map<String, String> filters) {
         return cardGrabber.getCards(filters);
     }
 
-    @GetMapping("/cards/{cardId}")
+    @GetMapping("/{cardId}")
     public Card getCardById(@PathVariable String cardId) {
         Card card = cardGrabber.getCardById(cardId);
         storageService.save(card);
--- src/main/java/com/parser/gwentdeckparser/api/CardsStorageController.java
+++ src/main/java/com/parser/gwentdeckparser/api/CardsStorageController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/cards")
+@RequestMapping("/storage")
 public class CardsStorageController {
 
     private final CardStorageMdbService storageService;
