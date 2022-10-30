70536d8e0c8eba41d85674010e94aefa9a2d54e6
Dmitry Kasyanov <dikanemo@gmail.com>
2022-10-30 23:14:32 +0100

    [DB:cards_translations]: change updates for cards documents; (#5)

--- src/main/java/com/parser/gwentdeckparser/cardStorage/service/CardDocConverter.java
+++ src/main/java/com/parser/gwentdeckparser/cardStorage/service/CardDocConverter.java
@@ -1,10 +1,11 @@
 package com.parser.gwentdeckparser.cardStorage.service;
 
 import com.parser.gwentdeckparser.cardStorage.model.CardDocument;
+import com.parser.gwentdeckparser.common.LocalisationEnum;
 import com.parser.gwentdeckparser.deckStructure.deckBuilder.Card;
 
 @FunctionalInterface
 public interface CardDocConverter {
 
-    CardDocument convert(Card card);
+    CardDocument convert(Card card, LocalisationEnum locale);
 }
